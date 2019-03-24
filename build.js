ec7e856f1d1bc0a4f89edbe322bda7542eea59fb
JuanCarlos <ajcarlos032@gmail.com>
2019-03-24 15:01:27 -0600

    Agregando Temas Variantes

--- build.js
+++ build.js
@@ -4,8 +4,14 @@ const tinycolor = require('tinycolor2');
 const yaml = require('js-yaml');
 
 const THEME_DIR = path.join(__dirname, 'theme');
+const THEME_YAML_FILE_ITALIC = path.join(__dirname, 'src', 'Italic.yml');
 const THEME_YAML_FILE = path.join(__dirname, 'src', 'JuanCarlos.yml');
-const THEME_YAML_FILE_SOFT = path.join(__dirname, 'src', 'JuanCarlos-Soft.yml');
+const THEME_YAML_FILE_BLACK = path.join(__dirname, 'src', 'JuanCarlosBlack.yml');
+const THEME_YAML_FILE_BLUE = path.join(__dirname, 'src', 'JuanCarlosDarkBlue.yml');
+const THEME_YAML_FILE_RED = path.join(__dirname, 'src', 'JuanCarlosDarkRed.yml');
+
+
+
 
 if (!fs.existsSync(THEME_DIR)) {
     fs.mkdirSync(THEME_DIR);
@@ -22,7 +28,13 @@ const withAlphaType = new yaml.Type('!alpha', {
 });
 const schema = yaml.Schema.create([withAlphaType]);
 const standard = fs.readFileSync(THEME_YAML_FILE, 'utf8');
-const soft = fs.readFileSync(THEME_YAML_FILE_SOFT, 'utf8');
+const italic = fs.readFileSync(THEME_YAML_FILE_ITALIC, 'utf8');
+const black = fs.readFileSync(THEME_YAML_FILE_BLACK, 'utf8');
+const blue = fs.readFileSync(THEME_YAML_FILE_BLUE, 'utf8');
+const red = fs.readFileSync(THEME_YAML_FILE_RED, 'utf8');
+
+
+
 
 yamlObj = yaml.load(standard, { schema });
 
@@ -49,6 +61,18 @@ fs.writeFileSync(
     JSON.stringify(yaml.load(standard, { schema }), null, 4)
 );
 fs.writeFileSync(
-    path.join(THEME_DIR, 'JuanCarlos-soft-IA.json'),
-    JSON.stringify(yaml.load(soft, { schema }), null, 4)
+    path.join(THEME_DIR, 'JuanCarlosItalic.json'),
+    JSON.stringify(yaml.load(italic, { schema }), null, 4)
+);
+fs.writeFileSync(
+    path.join(THEME_DIR, 'JuanCarlosBack.json'),
+    JSON.stringify(yaml.load(black, { schema }), null, 4)
+);
+fs.writeFileSync(
+    path.join(THEME_DIR, 'JuanCarlosDarkBlue.json'),
+    JSON.stringify(yaml.load(blue, { schema }), null, 4)
+);
+fs.writeFileSync(
+    path.join(THEME_DIR, 'JuanCarlosDarkRed.json'),
+    JSON.stringify(yaml.load(red, { schema }), null, 4)
 );
